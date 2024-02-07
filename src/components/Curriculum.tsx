e53e84edca29351ca9be4ea48c5167dd74de84fa
Alexis <alexisjrojas@hotmail.es>
2024-02-06 21:04:19 -0300

    parametros de curriculum actualizados 1.0

--- src/components/Curriculum.tsx
+++ src/components/Curriculum.tsx
@@ -1,15 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 
-export default function Curriculum({
-	setSection,
-}: {
-	setSection: Dispatch<
-		SetStateAction<{
-			movement: string;
-			index: number;
-		}>
-	>;
-}) {
+export default function Curriculum() {
 	return (
 		<div className="h-full w-full bg-[url('/CurriculumNew.jpg')] bg-contain  bg-center bg-no-repeat  bg-[#1a1a1a] rounded-sm border-2 border-quaternary"></div>
 	);
